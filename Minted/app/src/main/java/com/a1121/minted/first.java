deb88b9133b1c93e9637612364db0e8deff2a0c1
emma559 <emmaliu1206@gmail.com>
2017-11-26 18:41:47 +0800

    first commit

---
+++ Minted/app/src/main/java/com/a1121/minted/first.java
@@ -0,0 +1,13 @@
+package com.a1121.minted;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class first extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
